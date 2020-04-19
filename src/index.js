68cb5086312624af842a6d2359379d5de66f3301
Jennifer Qian <jq.22@dartmouth.edu>
2020-04-18 23:47:51 -0400

    push to starter

--- src/index.js
+++ src/index.js
@@ -1,21 +1,29 @@
-// import $ from 'jquery';
-
-
-// const $ = require('jquery');
-import $ from 'jquery';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import './style.scss';
 
-let num = 0;
+const App = () => <div className="test">All the REACT are belong to us! (This is incorrect grammar??)</div>;
 
-const timetracker = setInterval(
-  () => {
-    $('#main').html(`You've been on this page for ${num} seconds!`);
-    num += 1;
-  }, 1000,
-);
+ReactDOM.render(<App />, document.getElementById('main'));
 
-timetracker();
+// // import $ from 'jquery';
 
 
-/* got help from samiha datta regarding the order of declarations and functions. For some reason I declared my let variables
-after calling my functions, so the page kept saying "on this page for 'undefined/NaN' seconds" */
+// // const $ = require('jquery');
+// import $ from 'jquery';
+// import './style.scss';
+
+// let num = 0;
+
+// const timetracker = setInterval(
+//   () => {
+//     $('#main').html(`You've been on this page for ${num} seconds!`);
+//     num += 1;
+//   }, 1000,
+// );
+
+// timetracker();
+
+
+// /* got help from samiha datta regarding the order of declarations and functions. For some reason I declared my let variables
+// after calling my functions, so the page kept saying "on this page for 'undefined/NaN' seconds" */
